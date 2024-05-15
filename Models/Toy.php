f87783011699db2959d3bb734f84dccb9f643f09
DavideTessari <davtex@gmail.com>
2024-05-15 16:53:31 +0200

    complete toy and bed.php files

---
+++ Models/Toy.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/Product.php';
+
+class Toy extends Product {
+    public function __construct($image, $title, $price, $categoryIcon) {
+        parent::__construct($image, $title, $price, $categoryIcon, 'Gioco');
+    }
+}
+?>
